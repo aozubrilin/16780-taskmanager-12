8b095bdc1db39c55ddc419250d4618289316b118
aozubrilin <alex14_09@mail.ru>
2020-08-04 14:08:00 +0500

    -Правка по структуре(константа после импортов)

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-const TASK_COUNT = 3;
-
 import {createSiteMenuTemplate} from "./view/site-menu.js";
 import {createFilterTemplate} from "./view/filter.js";
 import {createTaskTemplate} from "./view/task.js";
 import {createTaskEditTemplate} from "./view/task-edit.js";
 import {createLoadMoreButtonTemplate} from "./view/load-more-button.js";
 import {createBoardTemplate} from "./view/board.js";
+
+const TASK_COUNT = 3;
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
